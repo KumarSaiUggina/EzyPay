f1f8c1bdd0db08ef2ebb2c64218b1014dc7d8b77
Kumar Sai Uggina <uggina.kumarsai@gmail.com>
2025-01-20 12:38:04 +0530

    Restructured layout components for better maintainability and readability

--- apps/user-app/AppbarClient.tsx
+++ apps/user-app/AppbarClient.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Appbar } from "@repo/ui/appbar";
 import { useRouter } from "next/navigation";
@@ -6,13 +6,16 @@ import { useRouter } from "next/navigation";
 export function AppbarClient() {
   const session = useSession();
   const router = useRouter();
-
   return (
-   <div>
-      <Appbar onSignin={signIn} onSignout={async () => {
-        await signOut()
-        router.push("/api/auth/signin")
-      }} user={session.data?.user} />
-   </div>
+    <div>
+      <Appbar
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut();
+          router.push("/home");
+        }}
+        user={session.data?.user}
+      />
+    </div>
   );
 }
--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,14 +1,16 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Providers } from "../provider";
-import { AppbarClient } from "../components/AppbarClient";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Providers } from '../provider';
+import ClientWrapper from '../ClientWrapper';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
-  description: "Simple wallet app",
+  title: 'Wallet',
+  description: 'Simple wallet app',
 };
 
 export default function RootLayout({
@@ -16,16 +18,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }): JSX.Element {
+  const showAppbarAndFooter = true; // default value
+
   return (
     <html lang="en">
-      <Providers>
-        <body className={inter.className}>
-          <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
-            <AppbarClient />
+      <body className={inter.className}>
+        <Providers>
+          <ClientWrapper>
+            <SpeedInsights />
+            <Analytics />
             {children}
-          </div>
-        </body>
-      </Providers>
+          </ClientWrapper>
+        </Providers>
+      </body>
     </html>
   );
 }
